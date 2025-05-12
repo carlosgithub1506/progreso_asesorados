740762d83094cea833d28ee86a47a74dd03e0e78
Carlos <carduardo15@gmail.com>
2025-05-12 01:17:24 -0300

    actualizando app

--- app_progreso.py
+++ app_progreso.py
@@ -142,4 +142,4 @@ if id_usuario:
       
 
     else:
-        st.error("❌ No se encontró un archivo para ese ID. Verificá el código ingresado o consultá con tu entrenador!.")
+        st.error("❌ No se encontró un archivo para ese ID. Verificá el código ingresado o consultá con tu entrenador!!!.")
